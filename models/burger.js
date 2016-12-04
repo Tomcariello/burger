45b0ebd34aeb481fc0a8568545f677ae60ae6ff4
Tomcariello <tomcariello@yahoo.com>
2016-12-04 16:13:01 -0500

    timing is not the problem. Tracking ORM path now

--- models/burger.js
+++ models/burger.js
@@ -13,7 +13,7 @@ var burger = {
 	// cols and vals are arrays
 	create: function (cols, vals, cb) {
 		console.log('creating burger in burger.js');
-		console.log('clog: ' + cols + "|" + val);
+		console.log('clog: ' + cols + "|" + vals);
 		orm.create('burgers', cols, vals, function (res) {
 			cb(res);
 		});
